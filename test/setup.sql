82941b152fb3c93a79b3784420aa78548cda03b0
Willem van Bergen <willem@vanbergen.org>
2011-09-01 15:52:18 -0400

    Be more forgiving in setting up and tearing down the tests.

--- test/setup.sql
+++ test/setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE test_table (id int, name varchar(100));
+CREATE TABLE IF NOT EXISTS test_table (id int, name varchar(100));
 CREATE PROJECTION test_table_p (id, name) AS SELECT * FROM test_table SEGMENTED BY HASH(id) ALL NODES OFFSET 1;
 INSERT INTO test_table VALUES (1, 'matt');
 COMMIT;
