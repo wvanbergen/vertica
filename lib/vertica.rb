2bcebfaaba3c6e55894c2ce6f1cea9d5543107c3
Willem van Bergen <willem@vanbergen.org>
2011-09-12 05:45:10 -0400

    Added quoting methods.

--- lib/vertica.rb
+++ lib/vertica.rb
@@ -11,6 +11,26 @@ module Vertica
   def self.connect(*args)
     Connection.new(*args)
   end
+  
+  def self.quote(value)
+    case value
+      when nil        then 'NULL'
+      when false      then 'FALSE'
+      when true       then 'TRUE'
+      when DateTime   then value.strftime("'%Y-%m-%d %H:%M:%S'::timestamp")
+      when Time       then value.strftime("'%Y-%m-%d %H:%M:%S'::timestamp")
+      when Date       then value.strftime("'%Y-%m-%d'::date")
+      when String     then "'#{value.gsub(/'/, "''")}'"
+      when BigDecimal then value.to_s('F')
+      when Numeric    then value.to_s
+      when Array      then value.map { |v| self.quote(v) }.join(', ')
+      else self.quote(value.to_s)
+    end
+  end
+  
+  def self.quote_identifier(identifier)
+    "\"#{identifier.to_s.gsub(/"/, '""')}\""
+  end
 end
 
 %w[
---
+++ test/unit/quoting_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class QuotingTest < Test::Unit::TestCase
+
+  def test_quote_identifier
+    assert_equal '"test"', Vertica.quote_identifier(:test)
+    assert_equal '"te""st"', Vertica.quote_identifier('te"st')
+    assert_equal '"te""""st"', Vertica.quote_identifier('te""st')
+  end
+
+  def test_quote
+    assert_equal "'test'", Vertica.quote('test')
+    assert_equal "'te''st'", Vertica.quote("te'st")
+    assert_equal "'te''''st'", Vertica.quote("te''st")
+    assert_equal "'test'", Vertica.quote(:test)
+
+    assert_equal 'NULL', Vertica.quote(nil)
+    assert_equal 'TRUE', Vertica.quote(true)
+    assert_equal 'FALSE', Vertica.quote(false)
+
+    assert_equal '1', Vertica.quote(1)
+    assert_equal '1.1', Vertica.quote(1.1)
+    assert_equal '1.1', Vertica.quote(BigDecimal.new('1.1'))
+
+    assert_equal "'2010-02-27'::date", Vertica.quote(Date.parse('2010-02-27'))
+    assert_equal "'2010-02-27 12:44:25'::timestamp", Vertica.quote(DateTime.parse('2010-02-27 12:44:25'))
+    assert_equal "'2010-02-27 12:44:25'::timestamp", Vertica.quote(Time.parse('2010-02-27 12:44:25'))
+    
+    assert_equal "NULL, 1, TRUE, 'test'", Vertica.quote([nil, 1, true, 'test'])
+  end
+end
