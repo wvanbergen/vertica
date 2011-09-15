bbf3831197d767a4c0d99896105298ac72e7a76c
Willem van Bergen <willem@vanbergen.org>
2011-09-15 06:52:11 -0400

    Fix a test to work in all Ruby versions.

--- test/unit/quoting_test.rb
+++ test/unit/quoting_test.rb
@@ -24,7 +24,7 @@ class QuotingTest < Test::Unit::TestCase
 
     assert_equal "'2010-02-27'::date", Vertica.quote(Date.parse('2010-02-27'))
     assert_equal "'2010-02-27 12:44:25'::timestamp", Vertica.quote(DateTime.parse('2010-02-27 12:44:25'))
-    assert_equal "'2010-02-27 12:44:25'::timestamp", Vertica.quote(Time.parse('2010-02-27 12:44:25'))
+    assert_equal "'2010-02-27 12:44:25'::timestamp", Vertica.quote(Time.utc(2010,2, 27, 12, 44, 25))
     
     assert_equal "NULL, 1, TRUE, 'test'", Vertica.quote([nil, 1, true, 'test'])
   end
