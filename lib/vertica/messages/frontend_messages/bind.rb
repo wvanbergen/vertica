b1795808565186db1bea31c56238e36ee92c408d
Willem van Bergen <willem@vanbergen.org>
2011-10-31 14:16:27 -0400

    Handle NULL values in Bind message properly

--- lib/vertica/messages/frontend_messages/bind.rb
+++ lib/vertica/messages/frontend_messages/bind.rb
@@ -6,12 +6,13 @@ module Vertica
       def initialize(portal_name, prepared_statement_name, parameter_values)
         @portal_name = portal_name
         @prepared_statement_name = prepared_statement_name
-        @parameter_values = parameter_values.map(&:to_s)
+        @parameter_values = parameter_values
       end
 
       def to_bytes
         bytes = [@portal_name, @prepared_statement_name, 0, @parameter_values.length].pack('Z*Z*nn')
-        bytes << @parameter_values.map { |val| [val.length, val].pack('Na*') }.join('') << [0].pack('n')
+        bytes << @parameter_values.map { |val| val.nil? ? [-1].pack('N') : [val.length, val].pack('Na*') }.join('')
+        bytes << [0].pack('n')
         message_string bytes
       end
     end
