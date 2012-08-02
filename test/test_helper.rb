b4e12a2f5e9d709680e5ddf530b70e8531816bc3
Willem van Bergen <willem@vanbergen.org>
2012-08-02 07:49:28 -0400

    Fail test suite gracefully if connection.yml is not available.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,14 @@ require 'test/unit'
 
 require 'vertica'
 
-TEST_CONNECTION_HASH = YAML.load(File.read(File.expand_path("../connection.yml", __FILE__)))
+TEST_CONNECTION_FILENAME = File.expand_path("../connection.yml", __FILE__)
+
+if File.exist?(TEST_CONNECTION_FILENAME)
+  TEST_CONNECTION_HASH = YAML.load(File.read(TEST_CONNECTION_FILENAME))
+else
+  puts
+  puts "Create #{TEST_CONNECTION_FILENAME} with connection info "
+  puts "for your Vertica test database in order to run the test suite."
+  puts
+  exit(1)
+end
