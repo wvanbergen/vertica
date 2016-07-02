7e7a1c38e5b6f58ea421e65ffbcd74aa56e6af70
Willem van Bergen <willem@vanbergen.org>
2016-07-02 13:56:29 -0400

    Add unit tests for Result class.

--- lib/vertica/result.rb
+++ lib/vertica/result.rb
@@ -24,30 +24,33 @@ class Vertica::Result
   alias_method :count, :size
   alias_method :length, :size
 
-  def [](row, col = nil)
-    row = rows.fetch(0)
+  def fetch(row_index, col = nil)
+    row = rows.fetch(row_index)
     return row if col.nil?
 
-    column = find_column(col)
+    column, index = find_column_with_index(col)
     case row
       when Hash; row.fetch(column.name)
-      when Array; row.fetch(column.attribute_number)
+      when Array; row.fetch(index)
     end
   end
 
+  alias_method :[], :fetch
+
   def value
-    self[0, 0]
+    fetch(0, 0)
   end
 
   alias_method :the_value, :value
 
   protected
 
-  def find_column(col)
-    case col
-      when Integer; columns.fetch(col)
-      when String, Symbol; columns.detect { |c| c.name.to_s == col }
+  def find_column_with_index(col)
+    index = case col
+      when Integer; col
+      when String, Symbol; columns.find_index { |c| c.name.to_s == col.to_s } or raise KeyError, "No column found with name #{col}"
       else raise ArgumentError, "#{col.inspect} is not a valid column identifier"
     end
+    [columns.fetch(index), index]
   end
 end
---
+++ test/unit/result_test.rb
@@ -0,0 +1,99 @@
+require 'test_helper'
+
+class ResultTest < Minitest::Test
+  def setup
+    @hash_result = Vertica::Result.new(
+      columns: [
+        Vertica::Column.new(name: 'a', data_type_oid: 6),
+        Vertica::Column.new(name: 'b', data_type_oid: 6),
+      ],
+      rows: [
+        { a: 1, b: 2},
+        { a: 3, b: 4},
+      ],
+      tag: 'SELECT'
+    )
+
+    @array_result = Vertica::Result.new(
+      columns: [
+        Vertica::Column.new(name: 'a', data_type_oid: 6),
+        Vertica::Column.new(name: 'b', data_type_oid: 6),
+      ],
+      rows: [
+        [1, 2],
+        [3, 4],
+      ],
+      tag: 'SELECT'
+    )
+  end
+
+  def test_basics
+    assert_equal 'SELECT', @hash_result.tag
+    assert_equal 'SELECT', @array_result.tag
+
+    assert_equal 2, @hash_result.size
+    assert_equal 2, @array_result.size
+
+    refute_predicate @hash_result, :empty?
+    refute_predicate @array_result, :empty?
+  end
+
+  def test_fetch_row
+    assert_equal Hash[:a, 1, :b, 2], @hash_result.fetch(0)
+    assert_equal Hash[:a, 3, :b, 4], @hash_result.fetch(1)
+
+    assert_equal [1, 2], @array_result.fetch(0)
+    assert_equal [3, 4], @array_result.fetch(1)
+
+    assert_raises(IndexError) { @hash_result.fetch(-1234) }
+    assert_raises(IndexError) { @hash_result.fetch(1234) }
+
+    assert_raises(IndexError) { @array_result.fetch(-1234) }
+    assert_raises(IndexError) { @array_result.fetch(1234) }
+  end
+
+  def test_fetch_row_with_negative_index
+    assert_equal @hash_result.fetch(0), @hash_result.fetch(-(@hash_result.size) + 0)
+    assert_equal @hash_result.fetch(1), @hash_result.fetch(-(@hash_result.size) + 1)
+  end
+
+  def test_fetch_value_by_column_index
+    assert_equal 1, @hash_result.fetch(0, 0)
+    assert_equal 2, @hash_result.fetch(0, 1)
+    assert_equal 1, @hash_result.fetch(0, -2)
+    assert_equal 2, @hash_result.fetch(0, -1)
+    assert_raises(IndexError) { @hash_result.fetch(0, 2) }
+    assert_raises(IndexError) { @hash_result.fetch(0, -3) }
+
+    assert_equal 1, @array_result.fetch(0, 0)
+    assert_equal 2, @array_result.fetch(0, 1)
+    assert_equal 1, @array_result.fetch(0, -2)
+    assert_equal 2, @array_result.fetch(0, -1)
+    assert_raises(IndexError) { @array_result.fetch(0, 2) }
+    assert_raises(IndexError) { @array_result.fetch(0, -3) }
+  end
+
+  def test_fetch_value_by_column_name
+    assert_equal 1, @hash_result.fetch(0, :a)
+    assert_equal 2, @hash_result.fetch(0, 'b')
+    assert_raises(KeyError) { @hash_result.fetch(0, :c) }
+
+    assert_equal 1, @array_result.fetch(0, :a)
+    assert_equal 2, @array_result.fetch(0, 'b')
+    assert_raises(KeyError) { @array_result.fetch(0, :c) }
+  end
+
+  def test_value
+    assert_equal @hash_result.value, @hash_result[0, 0]
+    assert_equal @array_result.value, @array_result[0, 0]
+  end
+
+
+  def test_enumerable
+    sum_a = @hash_result.inject(0) { |carry, row| carry + row[:a] }
+    sum_b = @array_result.inject(0) { |carry, row| carry + row[1] }
+
+    assert_equal 4, sum_a
+    assert_equal 6, sum_b
+  end
+end
