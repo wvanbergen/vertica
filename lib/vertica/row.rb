df0ddb83ff9557850ae96e3bf32450106270f48b
Willem van Bergen <willem@vanbergen.org>
2016-07-10 06:58:19 -0400

    Add documentation for the Vertica::Row class.

--- lib/vertica/row.rb
+++ lib/vertica/row.rb
@@ -1,16 +1,38 @@
+# Class to represent a row returns by a query.
+#
+# Row instances can either be yielded to the block passed to {Vertica::Connection#query},
+# or be part of a buffered {Vertica::Result} returned by {Vertica::Connection#query}.
+#
+# @attr_reader row_description [Vertica::RowDescription] The ordered list of columns this
+#   row conforms to.
+#
+# @see Vertica::RowDescription#build_row
 class Vertica::Row
   include Enumerable
 
   attr_reader :row_description
 
+  # Initializes a new row instance for a given row description and values. The
+  # number of values MUST match the number of columns in the row description.
+  # @param row_description [Vertica::RowDescription] The row description the
+  #   values will conform to.
+  # @param values [Array] The values for the columns in the row description
+  # @see Vertica::RowDescription#build_row
   def initialize(row_description, values)
     @row_description, @values = row_description, values
   end
 
+  # Iterates over the values in this row.
+  # @yield [value] The provided block will get called with the values in the order of
+  #   the columns in the row_description.
   def each(&block)
     @values.each(&block)
   end
 
+  # Fetches a value from this row.
+  # @param name_or_index [Symbol, String, Integer] The name of the column as string or symbol,
+  #   or the index of the column in the row description.
+  # @raise KeyError A KeyError is raised if the field connot be found.
   def fetch(name_or_index)
     @values.fetch(column_index(name_or_index))
   end
@@ -29,6 +51,8 @@ class Vertica::Row
   # @param symbolize_keys [true, false] Set to true to use symbols instead
   #   of strings for the field names.
   # @return [Hash]
+  # @raise [Vertica::Error::DuplicateColumnName] If the row contains multiple
+  #   columns with the same name
   def to_h(symbolize_keys: false)
     @row_description.inject({}) do |carry, column|
       key = symbolize_keys ? column.name.to_sym : column.name
@@ -38,16 +62,20 @@ class Vertica::Row
     end
   end
 
+  # @return [Boolean] Returns true iff this record is equal to the other provided object
   def eql?(other)
     other.kind_of?(Vertica::Row) && other.row_description == row_description && other.to_a == to_a
   end
 
   alias_method :==, :eql?
 
+  # @return [Integer] Returns a hash digtest of this object.
   def hash
     [row_description, values].hash
   end
 
+  # Returns a user-consumable string representation of this row.
+  # @return [String]
   def inspect
     "<Vertica::Row#{@values.inspect}>"
   end
