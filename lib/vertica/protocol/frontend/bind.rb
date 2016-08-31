b154431363351e32d35622d07e5a8e6898fceabe
Amir Tuval <amirtu@gmail.com>
2016-08-31 11:41:47 +0300

    correct bind request

--- lib/vertica/protocol/frontend/bind.rb
+++ lib/vertica/protocol/frontend/bind.rb
@@ -3,15 +3,18 @@ module Vertica
     class Bind < FrontendMessage
       message_id 'B'
 
-      def initialize(portal_name, prepared_statement_name, parameter_values)
+      def initialize(portal_name, prepared_statement_name, parameter_types, parameter_values)
         @portal_name = portal_name
         @prepared_statement_name = prepared_statement_name
+        @parameter_types = parameter_types
         @parameter_values = parameter_values
       end
 
       def message_body
-        bytes = [@portal_name, @prepared_statement_name, 0, @parameter_values.length].pack('Z*Z*nn')
-        bytes << @parameter_values.map { |val| val.nil? ? [-1].pack('N') : [val.length, val].pack('Na*') }.join('')
+        bytes = [@portal_name, @prepared_statement_name, @parameter_values.length].pack('Z*Z*n')
+        bytes << @parameter_values.map{0}.pack('n*')
+        bytes << [@parameter_types.length, *@parameter_types.map{|pt| pt.oid}].pack('nN*')
+        bytes << @parameter_values.map { |val| val.nil? ? [-1].pack('N') : [val.to_s.length, val.to_s].pack('Na*') }.join('')
         bytes << [0].pack('n')
       end
     end
