6ad0ee725ad66d4273c12a172174a064b77d1be4
Amir Tuval <amirtu@gmail.com>
2016-08-31 11:36:29 +0300

    fix parameter description handling

--- lib/vertica/protocol/backend/parameter_description.rb
+++ lib/vertica/protocol/backend/parameter_description.rb
@@ -8,7 +8,7 @@ module Vertica
       def initialize(data)
         parameter_count    = data.unpack('n').first
         parameter_type_ids = data.unpack("@2N#{parameter_count}")
-        @parameter_types   = parameter_type_ids.map { |id| Vertica::Column::DATA_TYPES[id] }
+        @parameter_types   = parameter_type_ids.map { |id| Vertica::DataType.build(oid: id) }
       end
     end
   end
