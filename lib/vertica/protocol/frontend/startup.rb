0b92659e6570ae5a9bf46a6f5897fe01883313ac
Sevag Hanssian <sevag.hanssian@gmail.com>
2016-12-12 18:43:55 -0500

    Add client info to connection

--- lib/vertica/protocol/frontend/startup.rb
+++ lib/vertica/protocol/frontend/startup.rb
@@ -8,12 +8,22 @@ module Vertica
         @user     = user
         @database = database
         @options  = options
+        @type     = "vertica-rb"
+        @pid      = Process.pid.to_s
+        @platform = Gem::Platform.local.to_s
+        @version  = Vertica::VERSION
+        @label    = @type+"-"+@version+"-"+SecureRandom.hex(10) 
       end
 
       def message_body
         str =  [Vertica::PROTOCOL_VERSION].pack('N')
         str << ["user", @user].pack('Z*Z*')         if @user
         str << ["database", @database].pack('Z*Z*') if @database
+        str << ["client_type", @type].pack('Z*Z*')
+        str << ["client_pid", @pid].pack('Z*Z*')
+        str << ["client_os", @platform].pack('Z*Z*')
+        str << ["client_version", @version].pack('Z*Z*')
+        str << ["client_label", @label].pack('Z*Z*')
         str << ["options", @options].pack('Z*Z*')   if @options
         str << [].pack('x')
       end
