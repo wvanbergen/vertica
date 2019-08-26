ca2d4961940ebdf03152dbf76489240460857cb8
Chris Jordan <chris.jordan@bluelabs.com>
2019-08-26 13:11:19 -0400

    ok some cargo culting

--- lib/vertica/protocol/backend/authentication.rb
+++ lib/vertica/protocol/backend/authentication.rb
@@ -4,6 +4,7 @@ module Vertica
       message_id 'R'
 
       OK                  = 0
+      KERBEROS_V4         = 1
       KERBEROS_V5         = 2
       CLEARTEXT_PASSWORD  = 3
       CRYPT_PASSWORD      = 4
@@ -11,18 +12,31 @@ module Vertica
       SCM_CREDENTIAL      = 6
       GSS                 = 7
       GSS_CONTINUE        = 8
-      SSPI                = 9
+      CHANGE_PASSWORD     = 9
+      PASSWORD_CHANGED    = 10
+      PASSWORD_GRACE      = 11
+      HASH                = 65536
+      HASH_MD5            = 65536 + 5
+      HASH_SHA512         = 65536+512
 
       attr_reader :code
       attr_reader :salt
+      attr_reader :userSalt
       attr_reader :auth_data
 
       def initialize(data)
         @code, other = data.unpack('Na*')
         puts "codes we received #{@code}, #{other}"
         case @code
-          when CRYPT_PASSWORD, MD5_PASSWORD then @salt = other
+          when CRYPT_PASSWORD then @salt = other
+          when MD5_PASSWORD, HASH_MD5 then @salt = other[4..other.size]
           when GSS_CONTINUE then @auth_data = other
+          when HASH, HASH_MD5
+            @salt =  other[4..other.size]
+            @userSaltLen = other[4..7].unpack('!I')[0]
+            puts "user salt length is #{@userSaltLen}"
+            @userSalt = other[8..other.size].unpack("!#{@userSaltLen}s")[0]
+            puts "user salt is #{@userSalt}"
         end
       end
     end
