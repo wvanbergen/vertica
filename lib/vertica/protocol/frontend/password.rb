1622c3b32a4c7c36b996fd53a4eede704257fe7a
Chris Jordan <chris.jordan@bluelabs.com>
2019-08-26 15:46:07 -0400

    teach password.rb to use sha512

--- lib/vertica/protocol/frontend/password.rb
+++ lib/vertica/protocol/frontend/password.rb
@@ -5,7 +5,7 @@ module Vertica
 
       def initialize(password, auth_method: Vertica::Protocol::Authentication::CLEARTEXT_PASSWORD, salt: nil, user: nil)
         @password = password
-        @auth_method, @salt, @user = auth_method, salt, user
+        @auth_method, @salt, @user, @userSalt = auth_method, salt, user, userSalt
       end
 
       def encoded_password
@@ -14,14 +14,18 @@ module Vertica
           @password
         when Vertica::Protocol::Authentication::CRYPT_PASSWORD
           @password.crypt(@salt)
-        when Vertica::Protocol::Authentication::MD5_PASSWORD, \
-             Vertica::Protocol::Authentication::HASH_MD5,     \
-             Vertica::Protocol::Authentication::HASH,         \
-             Vertica::Protocol::Authentication::HASH_SHA512
+        when Vertica::Protocol::Authentication::MD5_PASSWORD \
+             Vertica::Protocol::Authentication::HASH_MD5
           require 'digest/md5'
           @password = Digest::MD5.hexdigest("#{@password}#{@user}")
           @password = Digest::MD5.hexdigest("#{@password}#{@salt}")
           @password = "md5#{@password}"
+        when Vertica::Protocol::Authentication::HASH, \
+             Vertica::Protocol::Authentication::HASH_SHA512
+          require 'digest/sha512'
+          @password = Digest::SHA512.hexdigest("#{@password}#{@userSalt}")
+          @password = Digest::SHA512.hexdigest("#{@password}#{@salt}")
+          @password = "sha512#{@password}"
         else
           raise ArgumentError.new("unsupported authentication method: #{@auth_method}")
         end
