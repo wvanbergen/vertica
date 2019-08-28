4528717400b9c436e99c87f52814123570ea1426
Chris Jordan <chris.jordan@bluelabs.com>
2019-08-28 13:42:54 -0400

    some brute forcing

--- lib/vertica/protocol/backend/authentication.rb
+++ lib/vertica/protocol/backend/authentication.rb
@@ -35,6 +35,10 @@ module Vertica
             @salt =  other[0...4]
             @userSaltLen = other[4...8].unpack('n').first
             puts "user salt length is #{@userSaltLen}"
+            for i in 5..12 do 
+              puts "trying other range #{i}"
+              puts "#{other[4...i].unpack('n').first}"
+            end
             if @userSaltLen != 16
               puts "user salt length isn't 16, raise error"
             end 
