6bb1acc382e5892d2d4313aa50f3f1d8135827b9
Chris Jordan <chris.jordan@bluelabs.com>
2019-08-28 14:33:32 -0400

    print out manual hash

--- lib/vertica/protocol/frontend/password.rb
+++ lib/vertica/protocol/frontend/password.rb
@@ -27,6 +27,11 @@ module Vertica
           @password = Digest::SHA512.hexdigest("#{@password}#{@userSalt}")
           @password = Digest::SHA512.hexdigest("#{@password}#{@salt}")
           @password = "sha512#{@password}"
+          expected_password = Digest::SHA512.hexdigest("#{@userSalt}")
+          expected_password = Digest::SHA512.hexdigest("#{expected_password}#{@salt}")
+          expected_password = "sha512#{@expected_password}"
+          puts "         password is #{@password}"
+          puts "expected password is #{@expected_password}"
         else
           raise ArgumentError.new("unsupported authentication method: #{@auth_method}")
         end
